59bd9eb630a96165c33bf57132c0ddd092ec6b34
bulidiriba <bulidiriba@gmail.com>
2022-02-05 17:37:26 +0300

    implement put method to update the existing product

--- restful-product-api/handlers/products.go
+++ restful-product-api/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"example.com/m/data"
 )
@@ -28,6 +30,32 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	// to replace the existing product
+	if r.Method == http.MethodPut {
+		// expect the id in the URL
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI 1", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI 2", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to number", idString)
+			http.Error(rw, "Invalid URI 3", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("got id", id)
+		p.updateProducts(id, rw, r)
+	}
 
 	// to update some field of the existing product
 
@@ -55,4 +83,27 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	}
 	p.l.Printf("Prod: %#v", prod)
 	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Put requests")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	// if error found
+	if err != nil {
+		http.Error(rw, "Unable to decode the json value", http.StatusBadRequest)
+	}
+
+	er := data.UpdateProduct(id, prod)
+
+	if er == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if er == nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
 }
